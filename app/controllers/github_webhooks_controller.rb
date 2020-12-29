ac5c9a56c8cd0611f30d1ef4ff206c5e75e28388
Kevin Mulhern <kevinmulhern23@gmail.com>
2020-12-29 12:32:12 +0000

    WIP: Clean up controller

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -1,19 +1,13 @@
 class GithubWebhooksController < ActionController::Base
   include GithubWebhook::Processor
 
-  # Handle push event
   def github_push(payload)
     p payload
   end
 
-  # Handle create event
-  def github_create(payload)
-    # TODO: handle create webhook
-  end
-
   private
 
-  def webhook_secret(payload)
-    'Js_bQxVMW_WBkppf2nhN'
+  def webhook_secret(_)
+    ENV['GITHUB_WEBHOOK_SECRET']
   end
 end
